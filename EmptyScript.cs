6eae03c1217cef6d5a99cc6205699b20e0832a27
Yazan Qawasmeh <66473018+yazan929@users.noreply.github.com>
2021-06-13 13:13:48 +0800

    Update EmptyScript.cs

--- EmptyScript.cs
+++ EmptyScript.cs
@@ -16,31 +16,35 @@ public class EmptyScript : MonoBehaviour
 
     void Update()
     {
+    
         // for testing
 
         if (Input.GetKeyDown("space"))
         {
-            mm.MuteMusic("Menu");
+            mm.MuteMusic("Menu");  //plays all layers in Menu
         }
+        
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            mm.UnMuteMusic("Menu");
+            mm.UnMuteMusic("Menu");  //Unmutes all Layers in Menu
         }
+        
         if (Input.GetKeyDown(KeyCode.W))
         {
-            mm.UnMuteLayer("Menu", 0);
+            mm.UnMuteLayer("Menu", 0); //Unmutes layer number 0 in Menu
         }
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            mm.MuteLayer("Menu", 3);  //Mutes Layer 3 in Menu
+        }
+        
         if (Input.GetKeyDown(KeyCode.E))
         {
-
             mm.UnMuteLayer("Menu", 1);
         }
 
-        if (Input.GetKeyDown(KeyCode.R))
-        {
 
-            mm.MuteLayer("Menu", 3);
-        }
 
 
 
